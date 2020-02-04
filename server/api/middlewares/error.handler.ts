2dc8770a326754e15e9b99fecd4981d09eaf2069
Hans Jakob Emmel <hans.emmel@gmail.com>
2020-02-05 03:03:30 +1300

    Removing ts-node-dev

--- server/api/middlewares/error.handler.ts
+++ server/api/middlewares/error.handler.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import HttpException from "./http.exception";
+import { INTERNAL_SERVER_ERROR } from "http-status-codes";
 
 export default function errorHandler(
     error: HttpException,
@@ -8,7 +9,7 @@ export default function errorHandler(
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     next: NextFunction
 ) {
-    const status = error.status || 500;
+    const status = error.status || INTERNAL_SERVER_ERROR;
     const message = error.message || "Something went wrong";
     res.status(status).json({ message });
 }
--- server/common/openapi.ts
+++ server/common/openapi.ts
@@ -8,12 +8,19 @@ export default function openapi(
     routes: (app: Application) => void
 ) {
     const apiSpec = path.join(__dirname, "api.yml");
-    app.use(process.env.OPENAPI_SPEC || "/spec", express.static(apiSpec));
 
     new OpenApiValidator({
         apiSpec
-    }).install(app);
+    })
+        .install(app)
+        .then(() => {
+            app.use(
+                process.env.OPENAPI_SPEC || "/spec",
+                express.static(apiSpec)
+            );
+            routes(app);
+            app.use(errorHandler);
+        });
 
-    routes(app);
     app.use(errorHandler);
 }
